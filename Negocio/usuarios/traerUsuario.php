97f36675f9add780517a4754d55d15ee2fbd50ae
Mateo <mateomaslukinskas@gmail.com>
2025-10-17 11:03:35 -0300

    Corregir detalles de consultas

--- Negocio/usuarios/traerUsuario.php
+++ Negocio/usuarios/traerUsuario.php
@@ -2,12 +2,12 @@
 
 include_once "../Datos/Usuario.php";
 
-if (isset($_GET["idUsuario"]) && isset($_SESSION['usuarioRegistrado']) && $_SESSION['usuarioRegistrado'] === true) {
+if (isset($_SESSION["idUsuario"]) && isset($_SESSION['usuarioRegistrado']) && $_SESSION['usuarioRegistrado'] === true) {
     $usuario = new Usuario();
-    echo json_encode($usuario->traerUsuario($_GET["idUsuario"]));
+    echo json_encode($usuario->traerUsuario($_SESSION["idUsuario"]));
 } else {
     session_destroy();
-    echo "Error";
+    echo json_encode(["error" => "Sesión inválida o expirada"]);
 }
 
 ?>
